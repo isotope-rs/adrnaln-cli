35be6fa08a831bf647cf8a14ee5774f236f12f3f
Alex Jones <alexsimonjones@gmail.com>
2024-05-08 17:21:01 +0100

    chore: updated
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,99 @@
-
 use std::path::Path;
-
 
 use adrnaln::client::sequence::Sequence;
 use adrnaln::config::Addresses;
 use clap::{Parser, Subcommand};
 use tokio::fs;
 
-
-
 #[derive(Debug, clap::Args, Clone)]
 struct ClientArgs {
-    #[clap(short,long)]
+    #[clap(short, long)]
     ip: Option<String>,
     #[clap(short, long)]
     port: Option<String>,
     #[clap(short, long)]
-    file: Option<String>
+    file: Option<String>,
 }
 #[derive(Debug, clap::Args, Clone)]
 struct ServerArgs {
     #[clap(short, long)]
     port: Option<String>,
+    #[clap(short,long)]
+    download_directory: Option<String>
 }
 #[derive(Subcommand, Debug, Clone)]
 pub enum Mode {
     Server(ServerArgs),
-    Client(ClientArgs)
+    Client(ClientArgs),
 }
 #[derive(Parser, Debug)]
 #[command(version)]
 pub struct Args {
     #[command(subcommand)]
     mode: Mode,
-
 }
 #[tokio::main]
 async fn main() {
     let ars = Args::parse();
 
     match ars.mode {
-
         Mode::Server(args) => {
-
             if args.port.is_none() {
                 println!("Port is required");
-                return
+                return;
             }
             let (tx, mut rx) = tokio::sync::mpsc::channel::<Sequence>(100);
-            let (_kill_tx,kill_rx) = tokio::sync::oneshot::channel();
-            let config = adrnaln::config::Configuration{
-                addresses: Addresses { local_address: format!("0.0.0.0:{}", args.port.unwrap()).parse().unwrap(),
-                    remote_address: "0.0.0.0:0".parse().unwrap() },
+            let (_kill_tx, kill_rx) = tokio::sync::oneshot::channel();
+            let config = adrnaln::config::Configuration {
+                addresses: Addresses {
+                    local_address: format!("0.0.0.0:{}", args.port.unwrap()).parse().unwrap(),
+                    remote_address: "0.0.0.0:0".parse().unwrap(),
+                },
                 sequence_tx: tx,
             };
             let mut server = adrnaln::server::Server::new(config);
-            tokio::spawn(async move {
-               loop {
-                   match rx.recv().await {
-                       None => {}
-                       Some(seq) => {
-                          write_sequence_to_file(".",seq).await
-                       }
-                   }
+            tokio::spawn(
+                {
+                    let download_directory = args.download_directory.clone();
+                    async move {
+                        loop {
+                            match rx.recv().await {
+                                None => {}
+                                Some(seq) => {
+                                    let mut path = ".";
+                                    if !download_directory.is_none() {
+                                        path = download_directory.as_ref().unwrap().as_str();
+                                    }
+                                    write_sequence_to_file(path, seq).await
+                                },
+                            }
+                        }
+                    }
 
-               }
-            });
+                });
 
             server.start(kill_rx).await;
-
         }
         Mode::Client(args) => {
-            if args.port.is_none() || args.ip.is_none() || args.file.is_none(){
+            if args.port.is_none() || args.ip.is_none() || args.file.is_none() {
                 println!("Port, IP and File are required");
-                return
+                return;
             }
-            let addresses = Addresses { local_address: "0.0.0.0:0".parse().unwrap(),
-                remote_address: format!("{}:{}", args.ip.unwrap(), args.port.unwrap()).parse().unwrap() };
+            let addresses = Addresses {
+                local_address: "0.0.0.0:0".parse().unwrap(),
+                remote_address: format!("{}:{}", args.ip.unwrap(), args.port.unwrap())
+                    .parse()
+                    .unwrap(),
+            };
 
             let client = adrnaln::client::Client::new(addresses);
-            let sequence = client.build_sequence_from_file(args.file.unwrap().as_str()).await;
-            client.send_sequence(sequence.unwrap()).await.expect("Error sending sequence");
+            let sequence = client
+                .build_sequence_from_file(args.file.unwrap().as_str())
+                .await;
+            client
+                .send_sequence(sequence.unwrap())
+                .await
+                .expect("Error sending sequence");
         }
     }
 }
